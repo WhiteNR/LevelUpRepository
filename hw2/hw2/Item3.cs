c063ffee09dd8d36a50304856c0211f18da6aca6
WhiteNR <WhiteNR@gmail.com>
2013-10-14 20:47:26 +0300

    HW2 I3

---
+++ hw2/hw2/Item3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2
+{
+    class Item3
+    {
+        public void Start()
+        {
+            int A, B;
+            Console.WriteLine("A<B");
+            Console.WriteLine("Enter A:");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter B:");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = A; i <= B; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(i);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ hw2/hw2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item3 I3 = new Item3();
+            I3.Start();
+        }
+    }
+}

99fbe8c68d7b6b9799e844f2c663e58cb65902c6
Paul <ipaul.v@gmail.com>
2013-10-19 16:01:27 +0300

    Level up 18.10.2013

--- hw2/hw2/Program.cs
+++ hw2/hw2/Program.cs
@@ -10,10 +10,32 @@ namespace hw2
     {
         static void Main(string[] args)
         {
-             Item1.Start();
+             //Item1.Start();
              //Item2.Start();
              //Item3.Start();
              //Item4.Start();
+
+            string a = "";
+            do
+            {
+                Console.Write("Menu:\r\n1. Clear screen\r\n2. Say Hello!\r\n3. Exit\r\n\r\nPlease enter: ");
+
+                a = Console.ReadLine();
+                switch (a)
+                {
+                    case "1":
+                        Console.Clear();
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Hello");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        break;
+                }
+                Console.Clear();
+            } while (a != "3");
         }
     }
 }

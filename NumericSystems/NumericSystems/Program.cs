58a78daea6193b703cca7dbccc1c8b552b0fe11a
= <=>
2013-11-02 13:05:49 +0200

    my work project

---
+++ NumericSystems/NumericSystems/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumericSystems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, n = 0;
+            x = Convert.ToInt32(Console.ReadLine());
+             n = Convert.ToInt32(Console.ReadLine());
+            string str = ConverterOfNumericSystem(x, n);
+            Console.WriteLine(str);
+        }
+
+        public static string ConverterOfNumericSystem(int number, int numericSystem)
+        {
+            string str = "";
+
+            while (number % numericSystem < numericSystem)
+            {
+                str += (string)Convert.ToString(number / numericSystem);
+            }
+            return  str;
+        }
+    }
+}

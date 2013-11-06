5c39b86441d0dc238185382867d24215c8ff7451
WhiteNR <WhiteNR@gmail.com>
2013-11-07 00:26:51 +0200

    hw5

---
+++ hw5/hw5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Struct strct = new Struct();
+            strct.Parse("2.3");
+        }
+    }
+}
---
+++ hw5/hw5/Struct.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw5
+{
+    struct Struct
+    {
+        int a, b;
+
+        public void Parse(string str)
+        { 
+            string[] strArr;
+            char[] separator = {' ',','};
+            strArr = str.Split(separator,2);
+            if (strArr.Length == 2)
+            {
+                a = Int32.Parse(strArr[0]);
+                b = Int32.Parse(strArr[1]);
+            }
+            else
+            {
+                throw (new FormatException());
+            }
+        }
+
+        public int A { get { return a; } set { a = value; } }
+        public int B { get { return b; } set { b = value; } }
+    }
+}

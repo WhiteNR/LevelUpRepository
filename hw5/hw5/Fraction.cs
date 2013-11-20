15b0778d8d506ebb71657a647b26ce9a73e8e553
WhiteNR <WhiteNR@gmail.com>
2013-11-20 14:30:55 +0200

    hw5

--- hw5/hw5/Fraction.cs
+++ hw5/hw5/Fraction.cs
@@ -8,12 +8,55 @@ namespace hw5
 {
     class Fraction
     {
-        double result,a, b;
+        long a, b;
 
-        public Fraction(double x, double y)
+        public Fraction(long x, long y)
         {
             a = x;
             b = y;
+            divReduction(this);
+        }
+
+        private void divReduction(Fraction fraction)
+        {
+            long divisor = findGreatestCommonDivisor(fraction.a, fraction.b);
+
+            a = a / divisor;
+            b = b / divisor;
+        }
+
+        public override string ToString()
+        {
+            if (this.a == 0 || this.b == 0)
+            {
+                return String.Format("0");
+            }
+            else
+            {
+                return String.Format("{0}/{1}", this.a, this.b);
+            }
+        }
+
+        public static long findGreatestCommonDivisor(long a, long b)
+        {
+            while (b != 0)
+            {
+                long temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static Fraction ToDiv(double div)
+        {
+            long divCount = 1;
+            while (div % 1 > 0)
+            {
+                div *= 10;
+                divCount *= 10;
+            }
+            return new Fraction(Convert.ToInt64(div), divCount);
         }
 
         public static bool operator true(Fraction x)
@@ -27,17 +70,22 @@ namespace hw5
 
         public static Fraction operator *(Fraction x, double y)
         {
-            return new Fraction(x.a * y, x.b);
+            return x * ToDiv(y);
         }
 
         public static Fraction operator *(double x, Fraction y)
         {
-            return new Fraction(y.a * x, y.b);
+            return ToDiv(x) * y;
+        }
+
+        public static Fraction operator *(Fraction x, Fraction y)
+        {
+            return new Fraction(x.a * y.a, x.b * y.b);
         }
 
         public static Fraction operator +(Fraction x, double y)
         {
-            return new Fraction(x.a + y * x.b, x.b);
+            return x + ToDiv(y);
         }
 
         public static Fraction operator +(Fraction x, Fraction y)
@@ -47,7 +95,7 @@ namespace hw5
 
         public static Fraction operator -(Fraction x, double y)
         {
-            return new Fraction(x.a - y * x.b, x.b);
+            return x - ToDiv(y);
         }
 
         public static Fraction operator -(Fraction x, Fraction y)
@@ -57,13 +105,14 @@ namespace hw5
 
         public static Fraction operator /(Fraction x, double y)
         {
-            return new Fraction(x.a, x.b * y);
+            return x / ToDiv(y);
         }
 
         public static Fraction operator /(Fraction x, Fraction y)
         {
             return new Fraction(x.a * y.b, x.b * y.a);
         }
+
         public static bool operator ==(Fraction x, Fraction y)
         {
             return x.a == y.a && x.b == y.b;
--- hw5/hw5/Program.cs
+++ hw5/hw5/Program.cs
@@ -16,11 +16,11 @@ namespace hw5
             string str1, str2;
 
             //3
+            Console.WriteLine("Line:");
             Console.WriteLine("Please Enter A1,B1:");
             str1 = Console.ReadLine();
             Console.WriteLine("Please Enter A2 B2:");
             str2 = Console.ReadLine();
-
             //Console.WriteLine(coefforline.lineequation(3, -2, 1, 3, 4, 5));
 
             try
@@ -35,35 +35,41 @@ namespace hw5
                 Console.WriteLine(e.Message);
             }
             
-           
-
             //4
+            Console.WriteLine("Countries:");
             Kyiv kyiv = new Kyiv();
             Washington washington = new Washington();
             Moskow moskow = new Moskow();
-
-
-            //5
             if ((kyiv.Population > washington.Population) && (kyiv.Population > moskow.Population))
                 Console.WriteLine("Kyiv is bigger");
             else if ((washington.Population > kyiv.Population) && (washington.Population > moskow.Population))
                 Console.WriteLine("Washington is bigger");
             else
                 Console.WriteLine("Moskow is bigger");
+            Console.ReadLine();
             
             //6
+            Console.WriteLine("Complex:");
             Complex z = new Complex(1, 1);
             Complex z1;
             z1 = z - (z * z * z - 1) / (3 * z * z);
             Console.WriteLine("z1 ={0}", z1);
+            Console.ReadLine();
 
             //7
+            Console.WriteLine("Farction:");
             Fraction f = new Fraction(3, 4);
             int a = 10;
             Fraction f1 = f * a;
             Fraction f2 = a * f;
             double d = 1.5;
             Fraction f3 = f + d;
+
+            Console.WriteLine("f = {0}",f.ToString());
+            Console.WriteLine("f1 = {0}", f1.ToString());
+            Console.WriteLine("f2 = {0}", f2.ToString());
+            Console.WriteLine("f3 = {0}", f3.ToString());
+            Console.ReadLine();
 
         }
     }

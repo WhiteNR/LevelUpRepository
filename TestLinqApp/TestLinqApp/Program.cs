8152b978d8d6019bdf17d22f5e1c1b9f92f645c8
Syar <whitenr@gmail.com>
2014-01-12 16:04:37 +0200

    1

---
+++ TestLinqApp/TestLinqApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLinqApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new string[] { "A", "AS", "e", "R", "3", "67", "W", "a", "1", "34", "76" };
+
+            var result = from a in arr where a.StartsWith("A") select a;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
